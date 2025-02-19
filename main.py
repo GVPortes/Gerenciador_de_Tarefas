6ce162978b032a79afb72b9cbea70492e9ef92fc
Gabriel Portes <152006861+GVPortes@users.noreply.github.com>
2025-02-19 02:54:39 -0300

    Começando

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-tarefas =[]
-
+tarefas =['Abaixo seguem as tarefas',]
+print ("Bem vindo ao Gerenciador De Tarefas em Python")
 def adicionar_tarefa(tarefas):
-    tarefa = input('Digite a tarefa: ')
-    tarefas.append(tarefa)
-    print(f'Tarefa {tarefa} adicionada com sucesso!')
- 
- 
+    while True:
+        tarefa = input('Digite a tarefa: ')
+        tarefas.append(tarefa)
+        print(f'Tarefa {tarefa} adicionada com sucesso!')
+    
+        continuar = input('Deseja adicionar mais tarefas? (s/n) ')
+        if continuar == 'n':
+            break
+
+def remover_tarefa(tarefas):
+    while True:
+    #vamos ver se o usuário quer ou nao remover uma tarefa, se quiser, a gente mandar ele dizer qual, se nao a gente dá break.
+        if not tarefas:
+            print('Não há tarefas para remover!')
+            break
+        print('Tarefas disponíveis:')
+        for i, tarefa_atual in enumerate(tarefas, start=1):
+            print(f'{i} - {tarefa_atual }')
+        tarefa = int(input('Digite o número da tarefa que deseja remover: '))
+        tarefa.pop(tarefas - 1)
+        print(f'Tarefa {tarefa} removida com sucesso!')
+        continuar = input('Deseja remover mais tarefas? (s/n) ')
+        if continuar == 'n':
+            break
+
+adicionar_tarefa(tarefas)
+remover_tarefa(tarefas)
